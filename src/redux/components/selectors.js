e68332572010571d54789391e1eae826d854314a
denisolid <dkolisnichienko321@gmail.com>
2024-08-24 15:06:48 +0000

    14

--- src/redux/components/selectors.js
+++ src/redux/components/selectors.js
@@ -1,4 +1,19 @@
-export const selectContacts = (state) => state.contacts.items;
-export const selectNameFilter = (state) => state.filter.filter;
-export const selectIsError = (state) => state.contacts.isError;
-export const selectIsLoading = (state) => state.contacts.isLoading;
+import { createSelector } from "@reduxjs/toolkit";
+
+export const filteredContact = (state) => state.filter.filter;
+
+export const selectContacts = (state) => state.contact.contacts.items;
+
+export const selectIsLoading = (state) => state.contact.contacts.loading;
+
+export const selectError = (state) => state.contact.contacts.error;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, filteredContact],
+  (contacts, filtered) => {
+    const searchContact = contacts.filter((cont) =>
+      cont.name.toLowerCase().includes(filtered.toLowerCase())
+    );
+    return searchContact;
+  }
+);
